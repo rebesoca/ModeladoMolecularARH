898395bdf45930d47f7df08327bcd768573a8431
RebecaSoto <rebesoca@gmail.com>
2026-03-20 23:17:23 +0100

    Ficheros parte opcional

---
+++ 5-optional/submit_eck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH -p eck-q
+#SBATCH --chdir=/home/alumno26/mm/tareaRebecaSoto/5-optional
+#SBATCH -J equilibrado
+#SBATCH --cpus-per-task=1
+
+date
+gmx mdrun -deffnm arh -c arh.g96 -nt 1
+date
+
+
+
